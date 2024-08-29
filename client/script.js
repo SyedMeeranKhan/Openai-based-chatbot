1aa9c5cfbe7d963388e72087c6ef63f8f0311f00
Meeran <meerankham@gmail.com>
2024-08-30 01:38:19 +0500

    FEAT: FRONTEND almost readt

--- client/script.js
+++ client/script.js
@@ -35,6 +35,23 @@ function generateUniqueId() {
     const timestamp = DATE.now();
     const randomNumber = Math.random();
     const hexaDecimalString = randomNumber.toString(16);
-    
-    return `id-${timestamp}-${hexadecimalString}`;
+
+    return `id-${timestamp}-${hexaDecimalString}`;
+}
+
+function chatStripe(isAi,value, uniqueId){
+    return(
+        `
+            <div class="wrapper ${isAi && 'ai'}">
+                < class="chat">
+                    <div class="profile">
+                        <img src="${isAi ? bot : user}"
+                        alt="${isAi ? 'bot' : 'user'}"
+                        />
+                    </div>
+                    <div class="messsage" id=${uniqueId}>${value}</div>
+                </div>
+            </div>
+        `
+    )
 }
